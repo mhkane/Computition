f669388b3768f355d37fcadc12780ca75affac57
Hassan Kane <mhkane@fb.com>
2014-07-07 16:16:21 -0700

    Initial Commit

---
+++ Computition/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Computition
+//
+//  Created by Hassan Kane on 7/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ComputitionTests/ComputitionTests.m
@@ -0,0 +1,34 @@
+//
+//  ComputitionTests.m
+//  ComputitionTests
+//
+//  Created by Hassan Kane on 7/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ComputitionTests : XCTestCase
+
+@end
+
+@implementation ComputitionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
