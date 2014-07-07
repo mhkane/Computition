8238f830046ad3b892d0ec83ee8f9f28b0b2c8b7
Hassan Kane <mhkane@fb.com>
2014-07-07 16:41:17 -0700

    New files for testing
    Added BNRMath

---
+++ Computition/BNRMath.h
@@ -0,0 +1,13 @@
+//
+//  BNRMath.h
+//  Computition
+//
+//  Created by Hassan Kane on 7/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRMath : BNRCard
+
+@end
---
+++ Computition/BNRMath.m
@@ -0,0 +1,13 @@
+//
+//  BNRMath.m
+//  Computition
+//
+//  Created by Hassan Kane on 7/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRMath.h"
+
+@implementation BNRMath
+
+@end
