09f56f551fbacafeae9296147a49105182f88e94
Felipe Sinisterra <fsinisterra@fb.com>
2014-07-07 22:11:42 -0700

    Hope this works
    hopeful

---
+++ BNRModel.m
@@ -0,0 +1,32 @@
+//
+//  BNRModel.m
+//  Computition
+//
+//  Created by Felipe Sinisterra on 7/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRModel.h"
+
+@implementation BNRModel
+
++ (instancetype)randomEquation {
+    // Create an array that holds the first item
+    NSMutableArray *firstItem = [[NSMutableArray alloc] init];
+    //[firstItems addObject:];
+    
+    for (int i = 1; i <= 10; i++) {
+        BNRModel *equation = self.randomEquation;
+        [firstItem addObject:equation];
+    }
+    
+    // comiit
+    
+    // Create a second array that holds the second item
+    NSMutableArray *secondItem = [[NSMutableArray alloc] init];
+    // Create an array that holds the operators
+    
+}
+
+
+@end
