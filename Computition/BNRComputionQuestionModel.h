0ba7c34c25955392d0bfe5f48a1318f3b3ee1945
Felipe Sinisterra <fsinisterra@fb.com>
2014-07-07 17:04:55 -0700

    second test code
    self explanatory

--- Computition/BNRComputionQuestionModel.h
+++ Computition/BNRComputionQuestionModel.h
@@ -1,16 +1,13 @@
 //
-//  BNRMath.h
+//  BNRComputionQuestionModel.h
 //  Computition
 //
 //  Created by Hassan Kane on 7/7/14.
 //  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface BNRMath : BNRCard
-
-// Second test code
-
+@interface BNRComputionQuestionModel : NSObject
 
 @end
--- Computition/BNRComputionQuestionModel.m
+++ Computition/BNRComputionQuestionModel.m
@@ -1,16 +1,13 @@
 //
-//  BNRMath.h
+//  BNRComputionQuestionModel.m
 //  Computition
 //
 //  Created by Hassan Kane on 7/7/14.
 //  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "BNRComputionQuestionModel.h"
 
-@interface BNRMath : BNRCard
-
-// Second test code
-
+@implementation BNRComputionQuestionModel
 
 @end
