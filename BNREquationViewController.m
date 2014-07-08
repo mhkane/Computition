64238c3ac26084c5e6c083ecdfdccb09e8bc680d
Kayode Ezike <kezike@fb.com>
2014-07-07 17:52:07 -0700

    Computition Changes

---
+++ BNREquationViewController.m
@@ -0,0 +1,23 @@
+//
+//  BNREquationViewController.m
+//  Computition
+//
+//  Created by Kayode Ezike on 7/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNREquationViewController.h"
+
+@interface BNREquationViewController ()
+
+@property (nonatomic) NSString *question;
+- (void)updateQuestion;
+- (NSString)
+
+@end
+
+@implementation BNREquationViewController
+
+
+
+@end
