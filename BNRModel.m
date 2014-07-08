7073e44a77fd759db137bc50c3aa3f2688e69ba1
Felipe Sinisterra <fsinisterra@fb.com>
2014-07-08 09:07:28 -0700

    commit
    commit

--- BNRModel.m
+++ BNRModel.m
@@ -13,18 +13,25 @@
 + (instancetype)randomEquation {
     // Create an array that holds the first item
     NSMutableArray *firstItem = [[NSMutableArray alloc] init];
-    //[firstItems addObject:];
-    
-    for (int i = 1; i <= 10; i++) {
-        BNRModel *equation = self.randomEquation;
-        [firstItem addObject:equation];
+    for (NSInteger i = 0; i < 10; i++) {
+        [firstItem addObject:[NSNumber numberWithInt:i]];
     }
     
-    // comiit
-    
-    // Create a second array that holds the second item
+    // Create an array that holds the second item
     NSMutableArray *secondItem = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i < 10; i++) {
+        [secondItem addObject:[NSNumber numberWithInt:i]];
+    }
+   
     // Create an array that holds the operators
+    NSArray *operators = @[@"+", "-", "*", "/"];
+    
+    // Create a random first operand
+    
+    // Create a random second operand
+    
+    // Create a random operation
+    
     
 }
 
